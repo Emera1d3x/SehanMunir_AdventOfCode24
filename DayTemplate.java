7b694451f87f97902278a75a54bed8805159ef6c
Sehan Munir <162743866+Emera1d3x@users.noreply.github.com>
2024-12-10 09:52:54 -0500

    {The template I use for each day}
    I changed the template I use for each day after day 10.
    I did away with the buffered reader template I usually use for competitive programming because there is no need to get input quickly, and this code is much cleaner.

---
+++ DayTemplate.java
@@ -0,0 +1,21 @@
+
+/*
+ *   Sehan Munir
+ *   Day Template {2024-12-n}
+ *   Title
+ */
+import java.util.*;
+
+public class DayTemplate {
+  static Scanner scan = new Scanner(System.in);
+  public static void main(String[] args){
+    DayTemplate z = new DayTemplate();
+    System.out.println("Type \"-1\" after input is done: ");
+    // z.partOne();
+    // z.partTwo();
+  }
+  // PART ONE
+  static void partOne() {}
+  // PART TWO
+  static void partTwo() {}
+}
